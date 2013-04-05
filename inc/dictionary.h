c10e1648b671299e725357bcd298cbb543824708
naveen246 <naveen246@gmail.com>
2013-04-05 22:43:58 +0530

    added error.h

--- inc/dictionary.h
+++ inc/dictionary.h
@@ -6,15 +6,24 @@
 #include <set>
 #include <fstream>
 
+#include "error.h"
+
 class Dictionary {
     
     public :
 
         Dictionary( const std::string & fname ) {
         	std::ifstream wlist( fname.c_str() );
+        	if( ! wlist.is_open() ) {
+        		throw( ScheckError( "Could not open dictionary file " + fname ) );
+        	}
+
         	std::string word;
         	while( std::getline( wlist, word ) ) {
         		mWords.insert( word );
+        	}
+        	if( ! wlist.eof() ) {
+        		throw( ScheckError( "Error reading dictionary file " + fname ) );
         	}
         }
 
